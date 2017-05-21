753972b430b05233ee2626d98370eb12a81c8a85
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-05-21 17:17:34 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DOTFILES_DIR="$HOME/.dotfiles"
+
+# Do Symlinking.
+ln -sfv $DOTFILES_DIR/git/.gitconfig ~/.gitconfig
+ln -sfv $DOTFILES_DIR/zsh/.zshrc ~/.zshrc
+ln -sfv $DOTFILES_DIR/geany/geany.conf ~/.config/geany/geany.conf
+ln -sfv $DOTFILES_DIR/locale.conf ~/locale.conf
+ln -sfv $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
