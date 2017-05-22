0fc2d1fdd3fdcece991d5dffc6696a12f2fb1614
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-05-22 19:02:43 +0200

    Add install line for editorconf

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ ln -sfv $DOTFILES_DIR/zsh/.zshrc ~/.zshrc
 ln -sfv $DOTFILES_DIR/geany/geany.conf ~/.config/geany/geany.conf
 ln -sfv $DOTFILES_DIR/locale.conf ~/locale.conf
 ln -sfv $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
+ln -sfv $DOTFILES_DIR/.editorconfig ~/.editorconfig
