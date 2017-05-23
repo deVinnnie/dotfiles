6ff8a10fc50c4a3d9de2db9a9a075300b58b257f
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-05-23 16:52:21 +0200

    Add example secrets file

---
+++ git-bash/example-secrets.sh
@@ -0,0 +1,18 @@
+# OpenShift
+OC_USERNAME=
+OC_PASSWORD=
+
+declare -A OC_PROJECTS
+OC_PROJECTS["tennant-1"]="ravenclaw"
+OC_PROJECTS["tennant-2"]="gryffindor"
+
+# Jenkins
+JENKINS_PREFIX=
+JENKINS_SUFFIX=
+
+# Java Settings
+JAVA_ROOT=
+
+# Other
+PROJECTS_DIR=
+JAVA_ROOT=
