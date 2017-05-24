671080546c70443a713dc80fc04c4796c2e8a7ed
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-05-24 20:44:42 +0200

    Add Intellij Colour Scheme

--- install.sh
+++ install.sh
@@ -12,4 +12,8 @@ ln -sfv $DOTFILES_DIR/.editorconfig ~/.editorconfig
 ln -sfv $DOTFILES_DIR/atom/config.cson ~/.atom/config.cson
 ln -sfv $DOTFILES_DIR/atom/keymap.cson ~/.atom/keymap.cson
 
-
+cd $DOTFILES_DIR/intellij
+for file in `find . -type f -name \* -print`
+do
+    ln -sfv $DOTFILES_DIR/$file ~/.IntelliJIdea2017.1/config/$file
+done
