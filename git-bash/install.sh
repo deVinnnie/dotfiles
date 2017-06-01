38642f737c50346fb84740c9d91e21e48af3b8c8
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-06-01 11:24:19 +0200

    Include global gitignore file

--- git-bash/install.sh
+++ git-bash/install.sh
@@ -3,3 +3,5 @@ DOTFILES_DIR=`pwd`
 
 echo 'export DOTFILES='$DOTFILES_DIR > ~/.bashrc
 cat $DOTFILES_DIR/.bashrc >> ~/.bashrc
+
+git config --global core.excludesfile $DOTFILES_DIR/.gitignore_global
