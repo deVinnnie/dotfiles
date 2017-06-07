f6037c19383ec42f1561b2917ddc840aed9ba600
Pieter Jan Smets <pieter-jan.smets@smals.be>
2017-06-07 11:27:59 +0200

    Append to .bashrc instead of overwriting
    In order to not break any existing configuration, this commit changes
    the install script for git-bash to append to the .bashrc. It also
    adds a comment above to the newly introduced config to show where
    it came from.

--- git-bash/install.sh
+++ git-bash/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 DOTFILES_DIR=`pwd`
 
-echo 'export DOTFILES='$DOTFILES_DIR > ~/.bashrc
+echo '' >> ~/.bashrc
+echo '# Parameters for the dotfiles project' >> ~/.bashrc
+echo 'export DOTFILES='$DOTFILES_DIR >> ~/.bashrc
 cat $DOTFILES_DIR/.bashrc >> ~/.bashrc
 
 git config --global core.excludesfile $DOTFILES_DIR/.gitignore_global
