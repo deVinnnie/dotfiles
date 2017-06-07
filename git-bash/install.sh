7a766ef0585d5f94b040e2b3c73088169585e0ae
Pieter Jan Smets <pieter-jan.smets@smals.be>
2017-06-07 12:03:31 +0200

    Create secret.sh when running git-bash/install.sh
    After running the install script, the file is still missing. This
    commit makes sure the file exists and exports at least the
    PROJECTS_DIR variable.

--- git-bash/install.sh
+++ git-bash/install.sh
@@ -7,3 +7,9 @@ echo 'export DOTFILES='$DOTFILES_DIR >> ~/.bashrc
 cat $DOTFILES_DIR/.bashrc >> ~/.bashrc
 
 git config --global core.excludesfile $DOTFILES_DIR/.gitignore_global
+
+# Create secret.sh
+SECRET_FILE=secret.sh
+touch $SECRET_FILE
+read -p "Please enter the path of your projects directory: " PROJECTS_DIR
+echo "export PROJECTS_DIR=$PROJECTS_DIR" >> $SECRET_FILE
