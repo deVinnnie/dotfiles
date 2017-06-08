1413a72102bb9dea831f0b8d96d2ab5fdd93bcb4
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-06-08 10:52:48 +0200

    Move config file for findRepo to home dir
    * Change findRepo to read its config from ~/repos.yaml
    * Add a template for repos.yaml
    * Modify install script so it creates a new repos.yaml file based on the
      template upon first run.

--- git-bash/install.sh
+++ git-bash/install.sh
@@ -14,5 +14,7 @@ touch $SECRET_FILE
 read -p "Please enter the path of your projects directory: " PROJECTS_DIR
 echo "export PROJECTS_DIR=$PROJECTS_DIR" >> $SECRET_FILE
 
-touch findRepo.yaml
-echo 'Please fill in findRepo.yaml if you want to p to auto-download repos'
+if [ ! -f ~/repos.yaml ]; then
+  cp repos.template.yaml ~/repos.yaml
+  echo 'Please fill in repos.yaml if you want to p to auto-download repos'
+fi
