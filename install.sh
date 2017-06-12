9b742264289fcd1d022dbdd2cdd4f1c320c3d740
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-06-12 20:23:19 +0200

    Fix an issue with pulse

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DOTFILES_DIR="$HOME/.dotfiles"
 ln -sfv $DOTFILES_DIR/git/.gitconfig ~/.gitconfig
 ln -sfv $DOTFILES_DIR/zsh/.zshrc ~/.zshrc
 ln -sfv $DOTFILES_DIR/geany/geany.conf ~/.config/geany/geany.conf
+ln -sfv $DOTFILES_DIR/pulse/client.conf ~/.config/pulse/client.conf
 ln -sfv $DOTFILES_DIR/locale.conf ~/locale.conf
 ln -sfv $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
 ln -sfv $DOTFILES_DIR/.editorconfig ~/.editorconfig
