687646ea7088929f2fffa4dbf24a3fb27f77e044
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2018-03-16 21:54:20 +0100

    Add shared gradle props

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ ln -sfv $DOTFILES_DIR/.editorconfig ~/.editorconfig
 ln -sfv $DOTFILES_DIR/atom/config.cson ~/.atom/config.cson
 ln -sfv $DOTFILES_DIR/atom/keymap.cson ~/.atom/keymap.cson
 
+ln -sfv $DOTFILES_DIR/gradle/gradle.properties ~/.gradle/gradle.properties
+
 cd $DOTFILES_DIR/intellij
 for file in `find . -type f -name \* -print`
 do
