de6be520f4728ae697bc35fcaf80568b27ef6630
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2019-01-29 14:31:07 +0100

    Randomly change ports again

--- git-bash/docker-config.sh
+++ git-bash/docker-config.sh
@@ -1,5 +1,5 @@
 export DOCKER_CERT_PATH=
-export DOCKER_HOST=tcp://localhost:2375
+export DOCKER_HOST=tcp://localhost:2376
 export DOCKER_TLS_VERIFY=
 
 # Avoid 'Invalid bind mount spec' error when starting a container via docker-compose.
