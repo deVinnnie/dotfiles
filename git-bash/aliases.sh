fd1f9928f3df08e09c4f0e816bae1fef9156ca60
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2019-02-05 13:50:17 +0100

    Use rollout inst. of deploy

--- git-bash/aliases.sh
+++ git-bash/aliases.sh
@@ -1,6 +1,6 @@
 alias oc='oc.exe'
 alias ocl='oc.exe login -u '$OC_USERNAME' -p '$OC_PASSWORD
-alias ocd='oc.exe deploy $1 --latest'
+alias ocd='oc.exe rollout latest $1'
 alias ij='idea.sh'
 alias jenkins='$JAVA_HOME/jre/bin/java -jar /c/dev/common/jenkins-cli/jenkins-cli.jar -s $JENKINS_URL $@'
 alias compose-up='docker-compose up --force-recreate'
