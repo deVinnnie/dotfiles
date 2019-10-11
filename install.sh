110ca2d030245613386019cec53afa2325025ff3
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2019-10-11 22:10:21 +0200

    Remove geany

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ DOTFILES_DIR="$HOME/.dotfiles"
 # Do Symlinking.
 ln -sfv $DOTFILES_DIR/git/.gitconfig ~/.gitconfig
 ln -sfv $DOTFILES_DIR/zsh/.zshrc ~/.zshrc
-ln -sfv $DOTFILES_DIR/geany/geany.conf ~/.config/geany/geany.conf
 ln -sfv $DOTFILES_DIR/pulse/client.conf ~/.config/pulse/client.conf
 ln -sfv $DOTFILES_DIR/locale.conf ~/locale.conf
 ln -sfv $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
