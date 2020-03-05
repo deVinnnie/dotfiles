2a931fe56d4b29b50395e5afc5c3757470d04f9b
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2020-03-05 15:58:24 +0100

    Add new aliases

--- git-bash/aliases.sh
+++ git-bash/aliases.sh
@@ -6,6 +6,9 @@ alias jenkins='$JAVA_HOME/jre/bin/java -jar /c/dev/common/jenkins-cli/jenkins-cl
 alias refresh='source ~/.bashrc'
 alias l='ls'
 alias g='git $@'
+alias mvn='mvn.cmd $@'
+alias jq='/c/dev/tools/jq-win64.exe -C $@'
+alias t='tig status'
 
 # Change directory to root of the current git repo.
 alias root='cd "$(git rev-parse --show-toplevel)"' # https://stackoverflow.com/questions/1571461/git-a-quick-command-to-go-to-root-of-the-working-tree
