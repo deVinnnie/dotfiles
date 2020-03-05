e172c00410116514528499b8174b589e95597a54
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2020-03-05 15:51:28 +0100

    Remove unused aliases

--- git-bash/aliases.sh
+++ git-bash/aliases.sh
@@ -3,9 +3,7 @@ alias ocl='oc.exe login -u '$OC_USERNAME' -p '$OC_PASSWORD
 alias ocd='oc.exe rollout latest $1'
 alias ij='idea.sh'
 alias jenkins='$JAVA_HOME/jre/bin/java -jar /c/dev/common/jenkins-cli/jenkins-cli.jar -s $JENKINS_URL $@'
-alias compose-up='docker-compose up --force-recreate'
 alias refresh='source ~/.bashrc'
-alias parallel-mvn='mvn -T 1C $@'
 alias l='ls'
 alias g='git $@'
 
