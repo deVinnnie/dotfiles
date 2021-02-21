ab379e4e7f5e09a490b1588a665258c75a42efc8
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2021-02-21 16:19:59 +0100

    Restore colored output on Maven 3.6.3

--- git-bash/envs.sh
+++ git-bash/envs.sh
@@ -3,6 +3,8 @@ export VIMINIT='let $MYVIMRC="$DOTFILES/../vim/.vimrc" | source $MYVIMRC'
 # --------------------------
 # Maven
 # --------------------------
+# https://binkley.blogspot.com/2017/04/maven-color-logging-on-cygwin.html
+MAVEN_OPTS="-Djansi.passthrough=true"
 MAVEN_OPTS=$MAVEN_OPTS" -Xms1024m -Xmx4096m"
 MAVEN_OPTS=$MAVEN_OPTS" -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none"
 MAVEN_OPTS=$MAVEN_OPTS" -Djava.io.tmpdir=C:\\dev\\tmp"
