6d6ab4a9c8093edb6fc0e60333f5e024eb60668d
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2021-02-21 16:33:30 +0100

    Update docker config

--- git-bash/aliases.sh
+++ git-bash/aliases.sh
@@ -9,7 +9,8 @@ alias g='git $@'
 alias mvn='/c/dev/tools/apache-maven-3.6.3/bin/mvn.cmd $@'
 alias jq='/c/dev/tools/jq-win64.exe -C $@'
 alias t='tig status'
-
+alias docker='winpty docker $@' # Fixes 'The handle is invalid.' when running docker with interactive flag.
+alias real-docker='/c/Program\ Files/Docker/Docker/Resources/bin/docker $@' # Fixes fix when running docker in subshell
 # Change directory to root of the current git repo.
 alias root='cd "$(git rev-parse --show-toplevel)"' # https://stackoverflow.com/questions/1571461/git-a-quick-command-to-go-to-root-of-the-working-tree
 alias gitk='gitk $@ &'
--- git-bash/docker-config.sh
+++ git-bash/docker-config.sh
@@ -1,8 +1,5 @@
-export DOCKER_CERT_PATH=
-export DOCKER_HOST=tcp://localhost:2376
-export DOCKER_TLS_VERIFY=
-
 # Avoid 'Invalid bind mount spec' error when starting a container via docker-compose.
 # https://github.com/docker/compose/issues/4303
 # https://docs.docker.com/compose/reference/envvars/#compose_convert_windows_paths
-export COMPOSE_CONVERT_WINDOWS_PATHS=1
+export COMPOSE_CONVERT_WINDOWS_PATHS=0
+export COMPOSE_FORCE_WINDOWS_HOST=1
