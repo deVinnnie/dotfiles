6629b5485065b7d15ba0ec5c515fd45fe12f6f6b
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2022-01-10 22:10:03 +0100

    Remove intellij config
    Stuff is not dotfiles friendly and impossible to keep up.

--- install.sh
+++ install.sh
@@ -12,14 +12,6 @@ ln -sfv $DOTFILES_DIR/feh/themes ~/.config/feh/themes
 ln -sfv $DOTFILES_DIR/feh/keys ~/.config/feh/keys
 stow alacritty zathura newsboat
 
-
-mkdir ~/.IdeaIC2018.1/config/colors
-cd $DOTFILES_DIR/intellij
-for file in `find . -type f -name \* -print`
-do
-    ln -sfv $DOTFILES_DIR/$file ~/.IdeaIC2018.1/config/$file
-done
-
 ln -sfv $DOTFILES_DIR/bin/markdown-all.sh ~/bin/markdown-all.sh
 
 ln -sfv $DOTFILES_DIR/vim/.vimrc ~/.vimrc
