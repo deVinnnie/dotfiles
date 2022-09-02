85b5f63966fc1482b7120001c0317dbe4a05c216
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2022-09-02 19:19:52 +0200

    Put sway config in scm

--- system-install.sh
+++ system-install.sh
@@ -5,3 +5,6 @@ DOTFILES_DIR="$HOME/.dotfiles"
 echo "Setup Pacman Configuration"
 sudo cp $DOTFILES_DIR/system/etc/pacman.conf /etc/pacman.conf
 
+echo "Install udev rules"
+sudo cp $DOTFILES_DIR/system/udev/backlight.rules /etc/udev/rules.d/backlight.rules
+sudo udevadm control --reload-rules
