768b3e787b4ecf25c27c2f211f50289c42d1750b
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2022-09-22 16:11:13 +0200

    Show the title of the dashboard

--- git-bash/bin/jenkins-monitor.sh
+++ git-bash/bin/jenkins-monitor.sh
@@ -9,10 +9,15 @@ export YELLOW_ANIME='\e[33m '
 export ABORTED='  '
 export ABORTED_ANIME=' '
 export RESET='\e[0m'
+export BOLD='\e[97m'
 
 while :
 do
-    JOBS=$(curl -s $JENKINS_DASHBOARD \
+    BUILD_MONITOR_VIEW=$(curl -s $JENKINS_DASHBOARD)
+    TITLE=$(echo $BUILD_MONITOR_VIEW \
+        | jq-win64.exe --raw-output '.name')
+
+    JOBS=$(echo $BUILD_MONITOR_VIEW \
         | jq-win64.exe --raw-output '.jobs[] | "$" + (.color | ascii_upcase) + " " + .name + "$RESET"' \
         | sed "s/\$RESET/\\$RESET/g" \
         | sed "s/\$BLUE_ANIME/\\$BLUE_ANIME/g" \
@@ -24,6 +29,7 @@ do
         | sed "s/\$ABORTED_ANIME/\\$ABORTED_ANIME/g" \
         | sed "s/\$ABORTED/\\$ABORTED/g")
     clear
+    echo -e "$BOLD # $TITLE\n$RESET"
     echo -e "$JOBS"
     # envsubst scrambles special characters...
     #| envsubst '$BLUE $BLUE_ANIME $RED $RED_ANIME $YELLOW $YELLOW_ANIME $RESET'`
