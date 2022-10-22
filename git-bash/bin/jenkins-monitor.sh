b291d17e6d66e8cd8702bd1cd462fe87c3cf3301
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2022-10-22 21:15:40 +0200

    Support display of multiple dashboards

--- git-bash/bin/jenkins-monitor.sh
+++ git-bash/bin/jenkins-monitor.sh
@@ -11,14 +11,26 @@ export ABORTED_ANIME=' '
 export RESET='\e[0m'
 export BOLD='\e[97m'
 
-while :
-do
+function printDashboard
+{
+    JENKINS_DASHBOARD=$1
+    COLUMN=$2
+
     BUILD_MONITOR_VIEW=$(curl -s $JENKINS_DASHBOARD)
     TITLE=$(echo $BUILD_MONITOR_VIEW \
         | jq-win64.exe --raw-output '.name')
 
+    LENGTH=$(echo $BUILD_MONITOR_VIEW \
+        | jq-win64.exe --raw-output '.jobs | length')
+
+    EXCLUDE='nothing'
+    if [ $LENGTH -gt 20 ]; then
+        EXCLUDE='$BLUE '
+    fi
+
     JOBS=$(echo $BUILD_MONITOR_VIEW \
         | jq-win64.exe --raw-output '.jobs[] | "$" + (.color | ascii_upcase) + " " + .name + "$RESET"' \
+        | grep -v $EXCLUDE \
         | sed "s/\$RESET/\\$RESET/g" \
         | sed "s/\$BLUE_ANIME/\\$BLUE_ANIME/g" \
         | sed "s/\$BLUE/\\$BLUE/g" \
@@ -29,14 +41,59 @@ do
         | sed "s/\$NOTBUILT/\\$ABORTED/g" \
         | sed "s/\$ABORTED_ANIME/\\$ABORTED_ANIME/g" \
         | sed "s/\$ABORTED/\\$ABORTED/g")
-    clear
     echo -e "$BOLD # $TITLE\n$RESET"
     echo -e "$JOBS"
+
+    if [ $LENGTH -gt 20 ]; then
+        echo "   ..."
+    fi
+    echo -e "\n\n"
     # envsubst scrambles special characters...
     #| envsubst '$BLUE $BLUE_ANIME $RED $RED_ANIME $YELLOW $YELLOW_ANIME $RESET'`
+}
 
-    echo ""
-    echo "Hit [CTRL+C] to stop!"
-    sleep 30
+dashboards=($(echo $JENKINS_DASHBOARD | tr ';' ' '))
+WIDTH=45
+
+while :
+do
+    max_lines=0
+    col=0
+    line=0
+
+    # Set vars within loop to allow resizing
+    TOTAL_LINES=$(tput lines)
+    TOTAL_COLUMNS=$(( WIDTH * ( $(tput cols) / WIDTH) ))
+
+    # Only redraw once everything is calculated.
+    # Avoid stuttering.
+    BUFFER=''
+
+    for index in ${!dashboards[@]}; do
+        dashboard=${dashboards[$index]}
+        out=$(printDashboard $dashboard)
+
+        BUFFER=$BUFFER$(tput cup $line $col)
+        # https://unix.stackexchange.com/questions/405244/is-multi-line-alignment-possible-with-tput
+        BUFFER=$BUFFER$(echo "$out" | PREFIX=$(tput cr; tput cuf $col) awk '{print ENVIRON["PREFIX"] $0}')
+
+        curr_lines=$(echo "$out" | wc -l)
+        max_lines=$(( curr_lines > max_lines ? curr_lines : max_lines))
+
+        col=$(( ((index+1)*WIDTH) % TOTAL_COLUMNS ))
+        line=$(( ((index+1)*WIDTH / TOTAL_COLUMNS) * (max_lines+1) ))
+
+        if [ $line -gt $TOTAL_LINES ]; then
+            break
+        fi
+    done
+
+    tput clear
+    echo "$BUFFER"
+    if [ $line -gt $TOTAL_LINES ]; then
+        echo -n "Too many lines"
+    fi
+
+    sleep ${JENKINS_DASHBOARD_POLL_INTERVAL:-30}
 done
 
