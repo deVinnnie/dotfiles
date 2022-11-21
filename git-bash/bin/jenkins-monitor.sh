8f18d20aac14272b7cb55b908342f581367df217
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2022-11-21 09:44:51 +0100

    Support disabled state

--- git-bash/bin/jenkins-monitor.sh
+++ git-bash/bin/jenkins-monitor.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-export BLUE='\e[92m  '
-export BLUE_ANIME='\e[92m '
-export GREEN='\e[92m  '
-export RED='\e[91m  '
-export RED_ANIME='\e[91m '
-export YELLOW='\e[33m  '
-export YELLOW_ANIME='\e[33m '
-export ABORTED='  '
-export ABORTED_ANIME=' '
+export BLUE='\e[92m   '
+export BLUE_ANIME='\e[92m  '
+export GREEN='\e[92m   '
+export RED='\e[91m   '
+export RED_ANIME='\e[91m  '
+export YELLOW='\e[33m   '
+export YELLOW_ANIME='\e[33m  '
+export ABORTED='   '
+export ABORTED_ANIME='  '
+export DISABLED="   \\\e[9m"
 export RESET='\e[0m'
 export BOLD='\e[97m'
 
@@ -34,7 +35,7 @@ function printDashboard
     fi
 
     JOBS=$(echo $BUILD_MONITOR_VIEW \
-        | jq-win64.exe --raw-output '.jobs[] | "$" + (.color | ascii_upcase) + " " + .name + "$RESET"' \
+        | jq-win64.exe --raw-output '.jobs[] | "$" + (.color | ascii_upcase) + "" + .name + "$RESET"' \
         | grep -v $EXCLUDE \
         | sed "s/\$RESET/\\$RESET/g" \
         | sed "s/\$BLUE_ANIME/\\$BLUE_ANIME/g" \
@@ -45,7 +46,9 @@ function printDashboard
         | sed "s/\$YELLOW/\\$YELLOW/g" \
         | sed "s/\$NOTBUILT/\\$ABORTED/g" \
         | sed "s/\$ABORTED_ANIME/\\$ABORTED_ANIME/g" \
-        | sed "s/\$ABORTED/\\$ABORTED/g")
+        | sed "s/\$ABORTED/\\$ABORTED/g" \
+        | sed "s/\$DISABLED/\\$DISABLED/g"
+    )
     echo -e "$BOLD # $TITLE\n$RESET"
     echo -e "$JOBS"
 
